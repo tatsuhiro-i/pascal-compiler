3e8ef64cdd7db0ea1d0173b935ba36a51fbd38ad
09B17010 <09B17010@localhost>
2020-10-01 11:33:14 +0900

    message

--- src/main/java/enshud/s0/trial/Trial.java
+++ src/main/java/enshud/s0/trial/Trial.java
@@ -1,4 +1,9 @@
 package enshud.s0.trial;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Trial {
 
@@ -15,19 +20,32 @@ public class Trial {
 
 	/**
 	 * TODO
-	 * 
+	 *
 	 * 開発対象となるTrial実行メソッド （練習用）．
 	 * 以下の仕様を満たすこと．
-	 * 
+	 *
 	 * 仕様:
 	 * 第一引数で指定されたpascalファイルを読み込み，ファイル行数を標準出力に書き出す．
 	 * 入力ファイルが見つからない場合は標準エラーに"File not found"と出力して終了すること．
-	 * 
+	 *
 	 * @param inputFileName 入力pascalファイル名
 	 */
 	public void run(final String inputFileName) {
 
 		// TODO
+		try(BufferedReader in = new BufferedReader(new FileReader(new File(inputFileName)))){
+			String line;
+			int count = 0;
+			while((line = in.readLine()) != null) count ++ ;
 
+			System.out.println(count);
+		}catch(FileNotFoundException e) {
+			//e.printStackTrace();
+			System.out.println("File not found");
+			System.exit(-1);
+		}catch(IOException e) {
+			//e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 }
